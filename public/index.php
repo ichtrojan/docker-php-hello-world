8666b00076dbee150f4b95fdef680a02cbbb7eda
Michael okoh <ordrizzy@gmail.com>
2019-08-03 02:56:01 +0100

    New App

--- public/index.php
+++ public/index.php
@@ -2,18 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Docker PHP Hello World</title>
+    <title>Hello World</title>
   </head>
   <body>
-    <? if ($_POST['who'] != '') {?>
-      <h1>Hello <? echo htmlspecialchars($_POST['who']); ?></h1>
-      <a href="index.php">Greet someone else</a>
-    <? } else { ?>
-      <form class="greetingForm" action="index.php" method="post">
-        <label for="who">Say hello to</label>
-        <input type="text" name="who">
-        <input type="submit" name="greet" value="Say Hello">
-      </form>
-    <? } ?>
+    Hello World. the time is <?php echo date("h:i:sa"); ?>
   </body>
 </html>
