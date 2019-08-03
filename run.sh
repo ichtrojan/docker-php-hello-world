da73794e824c0575b733dd81408aeb7b7b398345
Michael okoh <ordrizzy@gmail.com>
2019-08-03 02:56:12 +0100

    New run command

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name=docker-php-hello-world -it --rm -p 90:80 docker-php-hello-world
+docker run --name=docker-php-hello-world -d --rm -p 90:80 docker-php-hello-world
